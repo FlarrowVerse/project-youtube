0188072e700ffbfc496d206938b8a38a3813ceb6
FlarrowVerse <archisman1999@gmail.com>
2022-07-03 15:28:52 +0530

    Modified Car. Added README.md.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Youtube
+
+These projects have been made from scratch following Youtube tutorials of these projects.
+
+The projects are listed below:
+
+## 1. Self-Driving Car Web Application:
+
+### Project Language: Javascript
--- SelfDriving_JS/main.js
+++ SelfDriving_JS/main.js
@@ -1,22 +1,25 @@
 const canvas = document.getElementById("root")
 
-canvas.width = 200;
+canvas.width = 400;
 
 const ctx = canvas.getContext("2d");
 
 const road = new Road(canvas.width/2, canvas.width * 0.95);
-const car = new Car(road.getLaneCenter(1), window.innerHeight, 15, 30);
+// const road2 = new Road(canvas.width*3/4, canvas.width/2 * 0.95);
+const car = new Car(road.getLaneCenter(1), window.innerHeight, 30, 60);
 
 animate();
 
 function animate() {
-    car.update();
+    // car.update([...road.borders, ...road2.borders]);
+    car.update(road.borders);
     canvas.height = window.innerHeight;
 
     ctx.save();
     ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
+    // road2.draw(ctx);
     car.draw(ctx);
 
     ctx.restore();
